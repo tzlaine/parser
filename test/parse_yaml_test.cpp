6f008cb19af71e34d7de36227a16bdbbf31823d4
Zach Laine <whatwasthataddress@gmail.com>
2017-04-21 23:01:05 -0500

    Use GTest in parse_yaml_test.cpp for better error reporting.

--- test/parse_yaml_test.cpp
+++ test/parse_yaml_test.cpp
@@ -9,32 +9,83 @@
 
 #include <yaml/parser/yaml.hpp>
 
+#include <gtest/gtest.h>
+
 #include <iostream>
 #include <fstream>
 
 
-///////////////////////////////////////////////////////////////////////////////
-//  Main program
-///////////////////////////////////////////////////////////////////////////////
-int main(int argc, char **argv)
+char const * filename = nullptr;
+std::ifstream * in_ptr = nullptr;
+
+
+TEST(parse, test_parse_yaml)
 {
-    char const* filename = NULL;
-    if (argc > 1)
+    ASSERT_TRUE(in_ptr != nullptr) << "failed to set up input file stream " << filename;
+
+    std::ifstream & in = *in_ptr;
+
+    using omd::yaml::ast::value_t;
+    using omd::yaml::parser::parse_yaml;
+    using omd::yaml::ast::print_yaml;
+
+    value_t result;
+    ASSERT_TRUE((parse_yaml(in, result, filename))) << "failed initial parse of " << filename;
+
+    print_yaml(std::cout, result);
+    std::cout << std::endl;
+
     {
-        filename = argv[1];
+        std::stringstream expanded_once;
+        omd::yaml::ast::print_yaml<3, true>(expanded_once, result);
+        value_t reparsed_once_result;
+        bool const expanded_once_parse =
+            parse_yaml(expanded_once, reparsed_once_result, filename + std::string("_expanded_once_string"));
+        EXPECT_TRUE(expanded_once_parse) << "reparse of expanded_once string failed!";
+        if (expanded_once_parse) {
+            std::stringstream expanded_twice;
+            omd::yaml::ast::print_yaml<3, true>(expanded_twice, result);
+            value_t reparsed_twice_result;
+            bool const expanded_twice_parse =
+                parse_yaml(expanded_twice, reparsed_twice_result, filename + std::string("_expanded_twice_string"));
+            EXPECT_TRUE(expanded_once_parse) << "reparse of expanded_twice string failed!";
+            if (expanded_twice_parse) {
+                EXPECT_EQ(reparsed_twice_result, reparsed_once_result) << "in parse -> print and expand -> parse -> print and expand -> parse, the final parse differs from middle parse!";
+            }
+        }
     }
-    else
+
+    {
+        std::stringstream unexpanded;
+        omd::yaml::ast::print_yaml(unexpanded, result);
+        value_t reparsed_result;
+        bool const unexpanded_parse =
+            parse_yaml(unexpanded, reparsed_result, filename + std::string("_unexpanded_string"));
+        EXPECT_TRUE(unexpanded_parse) << "reparse of unexpanded string failed!";
+        if (unexpanded_parse) {
+            EXPECT_EQ(reparsed_result, result) << "result of parse -> print and don't expand -> parse differs from initial parse!";
+        }
+    }
+}
+
+
+int main (int argc, char** argv)
+{
+    if (argc < 2)
     {
         std::cerr << "Error: No input file provided." << std::endl;
         return 1;
     }
 
+    // Last arg must be the name of the file to parse.
+    filename = argv[argc - 1];
     std::ifstream in(filename, std::ios_base::in);
+    in_ptr = &in;
 
     if (!in)
     {
         std::cerr << "Error: Could not open input file: "
-            << filename << std::endl;
+                  << filename << std::endl;
         return 1;
     }
 
@@ -48,54 +99,12 @@ int main(int argc, char **argv)
         if (s != std::string("\xef\xbb\xbf"))
         {
             std::cerr << "Error: Unexpected characters from input file: "
-                << filename << std::endl;
+                      << filename << std::endl;
             return 1;
         }
     }
 
-    using omd::yaml::ast::value_t;
-    using omd::yaml::parser::parse_yaml;
-    using omd::yaml::ast::print_yaml;
-
-    value_t result;
-    if (parse_yaml(in, result, filename))
-    {
-        print_yaml(std::cout, result);
-        std::cout << std::endl;
-
-        {
-            std::stringstream expanded_once;
-            omd::yaml::ast::print_yaml<3, true>(expanded_once, result);
-            value_t reparsed_once_result;
-            if (!parse_yaml(expanded_once, reparsed_once_result, filename + std::string("_expanded_once_string"))) {
-                std::cout << "error: reparse of expanded_once string failed!" << std::endl;
-            } else {
-                std::stringstream expanded_twice;
-                omd::yaml::ast::print_yaml<3, true>(expanded_twice, result);
-                value_t reparsed_twice_result;
-                if (!parse_yaml(expanded_twice, reparsed_twice_result, filename + std::string("_expanded_twice_string"))) {
-                    std::cout << "error: reparse of expanded_twice string failed!" << std::endl;
-                } else if (reparsed_twice_result != reparsed_once_result) {
-                    std::cout << "error: in parse -> print and expand -> parse -> print and expand -> parse, the final parse differs from middle parse!" << std::endl;
-                }
-            }
-        }
-
-        {
-            std::stringstream unexpanded;
-            omd::yaml::ast::print_yaml(unexpanded, result);
-            value_t reparsed_result;
-            if (!parse_yaml(unexpanded, reparsed_result, filename + std::string("_unexpanded_string"))) {
-                std::cout << "error: reparse of unexpanded string failed!" << std::endl;
-            } else if (reparsed_result != result) {
-                std::cout << "error: result of parse -> print and don't expand -> parse differs from initial parse!" << std::endl;
-            }
-        }
-    }
-    else
-    {
-        std::cout << "error: parse of original file failed!" << std::endl;
-    }
-
-    return 0;
+    int argc_ = argc - 1; // Don't give filename arg to GTest.
+    ::testing::InitGoogleTest(&argc_, argv);
+    return RUN_ALL_TESTS();
 }
