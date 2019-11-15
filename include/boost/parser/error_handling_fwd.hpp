bdd4514efd2250b39942170b065979c7bda55f13
Zach Laine <whatwasthataddress@gmail.com>
2019-11-15 00:00:16 -0600

    API documentation in parser/error_handling*.

--- include/boost/parser/error_handling_fwd.hpp
+++ include/boost/parser/error_handling_fwd.hpp
@@ -6,10 +6,16 @@
 
 namespace boost { namespace parser {
 
-    enum class error_handler_result { fail, rethrow };
+    /** The possible actions to take when a parse error is handled by an error
+        handler. */
+    enum class error_handler_result {
+        fail,   /// Fail the top-level parse.
+        rethrow /// Re-throw the parse error eexception.
+    };
 
-    // Contains an iterator to the point of failure, and the name of the
-    // failed parser or rule in what().
+    /** The exception thrown when a parse error is encountered, consisting an
+        iterator to the point of failure, and the name of the failed parser or
+        rule in `what()`. */
     template<typename Iter>
     struct parse_error : std::runtime_error
     {
@@ -21,6 +27,8 @@ namespace boost { namespace parser {
         Iter iter;
     };
 
+    /** A position within a line, consisting of an iterator to the start of
+        the line, the line number, and the column number. */
     template<typename Iter>
     struct line_position
     {
@@ -29,6 +37,8 @@ namespace boost { namespace parser {
         int64_t column_number;
     };
 
+    /** Writes a formatted message (meaning prefixed with the file name, line,
+        and column number) to `os`. */
     template<typename Iter>
     std::ostream & write_formatted_message(
         std::ostream & os,
@@ -40,6 +50,8 @@ namespace boost { namespace parser {
         int64_t preferred_max_line_length = 80,
         int64_t max_after_caret = 40);
 
+    /** Writes a formatted parse-expectation failure (meaning prefixed with
+        the file name, line, and column number) to `os`. */
     template<typename Iter>
     std::ostream & write_formatted_expectation_failure_error_message(
         std::ostream & os,
@@ -51,8 +63,31 @@ namespace boost { namespace parser {
         int64_t max_after_caret = 40);
 
     // TODO: Add info kind as well?
-    enum class diagnostic_kind { error, warning };
 
+    /** The kinds of diagnostics that can be handled by an error handler. */
+    enum class diagnostic_kind {
+        error,  /// An error diagnostic.
+        warning /// A warning diagnostic.
+    };
+
+    // TODO: error_handler concept.
+#if 0
+#if defined(__cpp_lib_concepts) && defined(__cpp_lib_ranges)
+    template<typename T>
+    BOOST_PARSER_CONCEPT cp_iterator =
+        ranges::forward_iterator<T> &&
+        requires (T & t, T const & ct) {
+            { *t } -> std::convertible_to<uint32_t>;
+            { *ct } -> std::convertible_to<uint32_t>;
+        };
+    template<typename T>
+    BOOST_PARSER_CONCEPT error_handler = requires (T const & t) {
+        
+    };
+#endif
+#endif
+
+    /** The error handler used when the user does not specify a custom one. */
     struct default_error_handler
     {
         constexpr default_error_handler() : os_(nullptr) {}
@@ -66,6 +101,10 @@ namespace boost { namespace parser {
             os_(&os)
         {}
 
+        /** Handles a `parse_error` exception thrown during parsing.  A
+            formatted parse-expectation failure is printed to `*os_` when
+            `os_` is non-null, or `std::cout` otherwise.  Always returns
+            `error_handler_result::fail`. */
         template<typename Iter>
         error_handler_result
         operator()(Iter first, Iter last, parse_error<Iter> const & e) const
@@ -76,6 +115,10 @@ namespace boost { namespace parser {
             return error_handler_result::fail;
         }
 
+        /** Prints `message` to `*os_` when `os_` is non-null, or `std::cout`
+            otherwise.  The diagnostic is printed with the given `kind`,
+            indicating the location as being at `it`.  This must be called
+            within a parser semantic action, providing the parse context. */
         template<typename Context, typename Iter>
         void diagnose(
             diagnostic_kind kind,
@@ -88,6 +131,10 @@ namespace boost { namespace parser {
                 os, filename_, _begin(context), it, _end(context), message);
         }
 
+        /** Prints `message` to `*os_` when `os_` is non-null, or `std::cout`
+            otherwise.  The diagnostic is printed with the given `kind`, at no
+            particular location.  This must be called within a parser semantic
+            action, providing the parse context. */
         template<typename Context>
         void diagnose(
             diagnostic_kind kind,
