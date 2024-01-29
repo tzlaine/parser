3d528fa53145e03c38304d86a9472780d207508b
Zach Laine <whatwasthataddress@gmail.com>
2024-01-28 21:23:06 -0600

    Remove mooted TODOs.

--- test/parser.cpp
+++ test/parser.cpp
@@ -1140,7 +1140,6 @@ TEST(parser, string_view)
             EXPECT_TRUE(prefix_parse(first, str.end(), parser, r));
             EXPECT_TRUE(r == U"");
         }
- #if 0 // TODO Odd failure on MSVC.
         {
             std::u32string const str = U"zs";
             range_t r;
@@ -1153,7 +1152,6 @@ TEST(parser, string_view)
             EXPECT_TRUE(parse(str, parser, r));
             EXPECT_TRUE(r == U"zszs");
         }
-#endif
         {
             std::u32string const str = U"";
             std::optional<range_t> result = parse(str, parser);
@@ -1173,7 +1171,6 @@ TEST(parser, string_view)
             EXPECT_TRUE(result);
             EXPECT_TRUE(*result == U"");
         }
-#if 0 // TODO: Same as above.
         {
             std::u32string const str = U"zs";
             std::optional<range_t> result = parse(str, parser);
@@ -1186,7 +1183,6 @@ TEST(parser, string_view)
             EXPECT_TRUE(result);
             EXPECT_TRUE(*result == U"zszs");
         }
-#endif
     }
 }
 #endif
